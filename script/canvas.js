84a1d61f88cd5e7e1027164910df056ffa056262
Rahul Goyal <rahul1120.be21@chitkara.edu.in>
2023-11-02 15:52:46 +0530

    Almost Final

--- script/canvas.js
+++ script/canvas.js
@@ -1,18 +1,51 @@
-var optionState = 0;
+var optionState = 1;
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 let drawing = false;
 
-function setState(newState){
-    optionState = newState;
-    const optionClicked = document.getElementById("options").querySelectorAll("h4");
-    if(optionState == 0){
-        optionClicked[0].classList.add("highlight");
-        optionClicked[1].classList.remove("highlight");
+setTimeout(() => {
+    const dataURL = localStorage.getItem("canvasState");
+    if (dataURL) {
+        const img = new Image();
+        img.src = dataURL;
+        img.onload = function () {
+            const context = canvas.getContext("2d");
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.drawImage(img, 0, 0);
+        };
     }
-    else{
-        optionClicked[1].classList.add("highlight");
-        optionClicked[0].classList.remove("highlight");
+}, 0)
+
+
+const spotLight = [
+    document.getElementById("CircleShape"), document.getElementById("brush"),
+    document.getElementById("eraser"), document.getElementById("SquareShape")
+];
+spotLight.forEach((headingElement) => {
+    headingElement
+    headingElement.addEventListener('click', () => setState(headingElement));
+});
+function setState(newState) {
+    spotLight.forEach((headingElement) => {
+        headingElement.classList.remove("highlight");
+    });
+    newState.classList.add("highlight");
+    switch (newState.id) {
+        case "eraser":
+            optionState = 0;
+            break;
+        case "brush":
+            optionState = 1;
+            break;
+        case "CircleShape":
+            optionState = 2;
+            break;
+        case "SquareShape":
+            optionState = 3;
+            break;
+        default:
+            console.log("Default state", newState.id, optionState);
+
     }
 }
 
@@ -25,38 +58,77 @@ canvas.addEventListener("mousedown", (e) => {
 });
 
 canvas.addEventListener("mousemove", (e) => {
-    if (!drawing) return;
+    if (optionState < 2 && drawing) drawAndRub(e);
+});
 
+document.addEventListener("mouseup", (e) => {
+    if (optionState == 2) {
+        drawCircle();
+    }
+    else if (optionState == 3) {
+        drawSquare();
+    }
+    drawing = false;
+});
+
+
+
+function drawAndRub(e) {
     let x = e.clientX - canvas.getBoundingClientRect().left;
     let y = e.clientY - canvas.getBoundingClientRect().top;
 
     context.beginPath();
     context.lineWidth = document.getElementById("brushSize").value;
     context.lineCap = "round";
-    context.strokeStyle = optionState ? "white" : document.getElementById("selectedColour").value;
+    context.strokeStyle = optionState == 0 ? "white" : document.getElementById("selectedColour").value;
     context.moveTo(lastX, lastY);
     context.lineTo(x, y);
     context.stroke();
 
     lastX = x;
     lastY = y;
-    
-});
-
-document.addEventListener("mouseup", () => (drawing = false));
-
-function clearCanvas() {
-    setState(0);
-    context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawCircle(){
+function clearCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    setState(document.getElementById("brush"));
+    saveCanvasState();
+}
+
+function drawCircle(r = 20) {
+    console.log("circle radius " + r);
     context.beginPath();
-    context.arc(200, 200, 50, 0, 2 * Math.PI);
-    context.fillStyle = "green"; // Fill color
+
+    context.arc(lastX, lastY, document.getElementById("brushSize").value * 2, 0, 2 * Math.PI);
+    context.fillStyle = "white";
     context.fill();
-    context.strokeStyle = "black"; // Border color
-    context.lineWidth = 5; // Border width
+    context.strokeStyle = document.getElementById("selectedColour").value;
+    context.lineWidth = 5;
     context.stroke();
     context.closePath();
 }
+
+function drawSquare() {
+    const a = 50;
+    context.strokeStyle = document.getElementById("selectedColour").value;
+    context.fillStyle = "white";
+
+    // Draw the square
+    context.beginPath();
+    context.rect(lastX, lastY, a, a);
+    context.closePath();
+    context.stroke();
+    context.fill();
+
+}
+
+
+function saveCanvasState() {
+    const dataURL = canvas.toDataURL();
+    localStorage.setItem("canvasState", dataURL);
+}
+
+// Asynchronus function
+setInterval(saveCanvasState, 5000);
+
+//This above function i used for saving canvas state every 5 seconds
